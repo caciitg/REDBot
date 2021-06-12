0bb4aae111fd46e9ea2c9254ff1196791ef8a0b5
Debarshi Chanda <58569158+DebarshiChanda@users.noreply.github.com>
2021-06-12 06:10:03 +0530

    Update download_weights.py

--- download_weights.py
+++ download_weights.py
@@ -2,7 +2,7 @@ import os
 import gdown
 
 
-os.makedirs("new_checkpoints", exist_ok=True)
+os.makedirs("checkpoints", exist_ok=True)
 url = "https://drive.google.com/uc?id=1GMj0u-EmawLpqKQ8NQJh_PC478aimG9D"
 destination = "checkpoints/model.bin"
 gdown.download(url, output=destination)
